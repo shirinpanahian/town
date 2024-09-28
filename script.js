d19cc77c72b5c60fc498bb8f5bdc0e11dfa4873c
shirinpanahian <sh.panahian@gmail.com>
2024-09-28 13:17:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+  const images = [
+                 'images/Isfahan.jpg',
+                 'images/isfahan2.jpg',
+                 'images/isfahan3.jpg'
+            ];
+            let currentIndex = 0;
 
+            const hero = document.getElementById('hero');
+
+            function updateBackground() {
+            hero.style.backgroundImage = `url(${images[currentIndex]})`;
+            }
+
+// Initial background
+            updateBackground();
+
+// Change background every 3 seconds
+            setInterval(() => {
+            currentIndex = (currentIndex + 1) % images.length;
+            updateBackground();
+               }, 3000);
